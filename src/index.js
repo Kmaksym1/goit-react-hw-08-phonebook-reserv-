32ca286aa55deb6a6da29b91c3231e6090849599
Maksym Kravchenko <kmaksym@gmail.com>
2023-07-27 17:57:28 -0700

    07/27

--- src/index.js
+++ src/index.js
@@ -21,3 +21,25 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
+
+
+// "@auth0/auth0-react": "^2.2.0",
+// "@chakra-ui/icons": "^2.1.0",
+// "@chakra-ui/react": "^2.8.0",
+// "@emotion/react": "^11.11.1",
+// "@emotion/styled": "^11.11.0",
+// "@reduxjs/toolkit": "^1.9.5",
+// "axios": "^1.4.0",
+// "formik": "^2.4.2",
+// "framer-motion": "^10.13.0",
+// "react": "^18.2.0",
+// "react-dom": "^18.1.0",
+// "react-loader-spinner": "^5.3.4",
+// "react-redux": "^8.1.1",
+// "react-router-dom": "^6.14.2",
+// "react-scripts": "5.0.1",
+// "react-toastify": "^9.1.3",
+// "redux": "^4.2.1",
+// "redux-persist": "^6.0.0",
+// "web-vitals": "^2.1.3",
+// "yup": "^1.2.0"
